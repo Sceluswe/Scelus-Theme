993dd4c6cab539459f04a608b60ce06a626c42ac
Sceluswe <lord_akiminos@hotmail.com>
2015-11-24 00:27:30 +0100

    Uploaded the theme.
    Made sure everyone is given credit for their work and cleaned up some of
    the code.

---
+++ test.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<meta charset='utf-8'/>
+<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<title>Test</title>
+<link rel='stylesheet' type='text/css' href='style.php'/>
+<link rel='icon' href='img/favicon.png'
+</head>
+
+<body>
+<div id='wrapper'>
+	<div id='header'>
+		<img style='height:128px;' class='sitelogo' src='img/favicon.png' alt='There is no test logo'/>
+		<h1 class='sitetitle'>Scelus-theme</h1>
+		<p class='siteslogan'>Simple and responsive</p>
+	</div>
+
+	<div id='navbar'>
+		<nav class='navbar'>
+			<ul>
+				<li><a href='test.php'>Test1</a></li>
+				<li><a href='test.php'>Test2</a></li>
+				<li><a href='test.php'>Test3</a></li>
+				<li><a href='test.php'>Test4</a></li>
+			</ul>
+		</nav>
+	</div>
+
+	<div id='content'>
+		<div id='flash'>
+			<h1>Flash</h1>
+			<p>This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text. This is a test text that I use to test the text.</p>
+		</div>
+		
+		<div id='wrap-featured'>
+			<div id='featured-1'>
+				<h1>Featured-1</h1>
+				<p>This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.</p>
+			</div>
+			<div id='featured-2'>
+				<h1>Featured-2</h1>
+				<p>This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.</p>
+			</div>
+			<div id='featured-3'>
+				<h1>Featured-3</h1>
+				<p>This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.</p>
+			</div>
+		</div>
+		
+		<div id='main'>
+		<h1>Main</h1>
+		<p>This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text. This is a test text that I use to test the text.</p>
+		</div>
+		
+		<div id='wrap-triptych'>
+			<div id='triptych-1'>
+				<h1>Triptych-1</h1>
+				<p>This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text. This is a test text that I use to test the text.</p>
+			</div>
+			<div id='triptych-2'>
+				<h1>Triptych-2</h1>
+				<p>This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text. This is a test text that I use to test the text.</p>
+			</div>
+			<div id='triptych-3'>
+				<h1>Triptych-3</h1>
+				<p>This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text. This is a test text that I use to test the text.</p>
+			</div>
+		</div>
+		
+		<div id='wrap-footer-col'>
+			<div id='footer-col-1'>
+				<h1>Footer-col-1</h1>
+				<p>This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.</p>
+			</div>
+			<div id='footer-col-2'>
+				<h1>Footer-col-2</h1>
+				<p>This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.</p>
+			</div>
+			<div id='footer-col-3'>
+				<h1>Footer-col-3</h1>
+				<p>This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.</p>
+			</div>
+			<div id='footer-col-4'>
+				<h1>Footer-col-4</h1>
+				<p>This is a test text that I use to test the text. This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.This is a test text that I use to test the text.</p>
+			</div>
+		</div>
+
+	</div>
+
+	<footer id='footer'>
+	&copy; Emil 'Scelus' Mattsson
+	</footer>
+</div>
+</body>
+
+</html>
